f0cf93c14e7e1611223b1acdcf8cc40705ed4c51
马俊杰 <majunjie@autohome.com.cn>
2018-07-06 14:40:14 +0800

    register

--- routes/index.js
+++ routes/index.js
@@ -56,23 +56,37 @@ router.route("/register").get(function(req,res){    // 到达此路径则渲染r
   var upwd = req.body.upwd;
   User.findOne({name: uname},function(err,doc){   // 同理 /login 路径的处理方式
     if(err){ 
-      res.send(500);
+      res.send({
+        code: 500,
+        message: '网络异常错误！'
+      });
       req.session.error =  '网络异常错误！';
       console.log(err);
     }else if(doc){ 
       req.session.error = '用户名已存在！';
-      res.send(500);
+      res.send({
+        code: 500,
+        message: '用户名已存在！'
+      });
     }else{ 
       User.create({                             // 创建一组user对象置入model
         name: uname,
         password: upwd
       },function(err,doc){ 
         if (err) {
-          res.send(500);
+          res.send(
+            {
+              code: 500,
+              message: '用户名创建失败！'
+            }
+          );
           console.log(err);
         } else {
           req.session.error = '用户名创建成功！';
-          res.send(200);
+          res.send({
+            code: 200,
+            message: '用户名创建成功！'
+          });
         }
       });
     }
