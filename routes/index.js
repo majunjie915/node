51a735a7a68e912b5dc1d4f76a685ba04829a7c6
马俊杰 <majunjie@autohome.com.cn>
2018-07-06 14:28:52 +0800

    login

--- routes/index.js
+++ routes/index.js
@@ -15,21 +15,31 @@ router.route("/login").get(function(req,res){    // 到达此路径则渲染logi
   var User = global.dbHandel.getModel('user');  
   var uname = req.body.uname;                //获取post上来的 data数据中 uname的值
   User.findOne({name:uname},function(err,doc){   //通过此model以用户名的条件 查询数据库中的匹配信息
+    console.log(111, doc);
     if(err){                                         //错误就返回给原post处（login.html) 状态码为500的错误
       res.send(500);
       console.log(err);
     }else if(!doc){                                 //查询不到用户名匹配信息，则用户名不存在
       req.session.error = '用户名不存在';
-      res.send(404);                            //    状态码返回404
+      res.send({
+        code: 401,
+        message: '用户名不存在'
+      });                            //    状态码返回404
     //    res.redirect("/login");
     }else{ 
       if(req.body.upwd != doc.password){     //查询到匹配用户名的信息，但相应的password属性不匹配
         req.session.error = "密码错误";
-        res.send(404);
+        res.send({
+          code: 402,
+          message: '密码错误'
+        });
       //    res.redirect("/login");
       }else{                                     //信息匹配成功，则将此对象（匹配到的user) 赋给session.user  并返回成功
         req.session.user = doc;
-        res.send(200);
+        res.send({
+          code: 200,
+          message: '登录成功'
+        });
       //    res.redirect("/home");
       }
     }
@@ -77,7 +87,7 @@ router.get("/home",function(req,res){
   }
   res.render("home",{
     title:'Home',
-    user: {name: 'Tom'}
+    user: {name: req.session.user.name}
   });         //已登录则渲染home页面
 });
 
