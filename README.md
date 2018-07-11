986f58065adde7cb17d0752d721a02e0f9ec7ace
马俊杰 <majunjie@autohome.com.cn>
2018-07-11 17:24:55 +0800

    添加注释

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 #若不想每次修改代码后都要重启服务器，可以安装并使用 supervisor 工具实现代码修改和自启动
 npm install -g supervisor
 supervisor bin/www
+
+multer 用于文件上传，目前引入了但没用到
--- app.js
+++ app.js
@@ -1,37 +1,42 @@
+// 加载依赖库
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var multer = require('multer');
+var mongoose = require('mongoose');
+var session = require('express-session');
 
+// 加载路由
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var multer = require('multer');
-var mongoose = require('mongoose');
-
-var session = require('express-session');
-
+// 项目实例化
 var app = express();
 
 app.use(session({ 
   secret: 'secret',
   cookie:{ 
-    maxAge: 1000*60*30,
+    maxAge: 1000 * 60 * 30,
   }
 }));
-// view engine setup
+// view engine setup （设置模板位置和模板引擎格式）
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').__express);
 app.set('view engine', 'html');
 
+// 定义日志和输出级别
 app.use(logger('dev'));
-app.use(express.json());
+// 定义数据解析器 基于 body-parser
+app.use(express.json()); 
 app.use(express.urlencoded({ extended: false }));
+// 定义 cookie 解析器
 app.use(cookieParser());
-app.use(cookieParser());
+// 定义静态文件目录
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 定义匹配路由
 app.use('/', indexRouter); // 为 / 设置路由
 app.use('/users', usersRouter); // 为 /user 设置路由
 app.use('/login',indexRouter); // 即为为路径 /login 设置路由
@@ -39,7 +44,7 @@ app.use('/register',indexRouter); // 即为为路径 /register 设置路由
 app.use('/home',indexRouter); // 即为为路径 /home 设置路由
 app.use("/logout",indexRouter); // 即为为路径 /logout 设置路由
 
-// catch 404 and forward to error handler
+// catch 404 and forward to error handler（处理404错误）
 app.use(function(req, res, next) {
   next(createError(404));
 });
